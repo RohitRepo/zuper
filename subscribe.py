c7aa9da106778a4e58a69dfa419969ba005d3a8f
Rohit <rohitsrepo@gmail.com>
2016-07-26 15:29:52 +0530

    Adding start for presence listener

--- subscribe.py
+++ subscribe.py
@@ -2,5 +2,6 @@ import os, django, shutil
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zuper.settings")
 django.setup()
 
-from notifications.pubsub import subscribe_global
+from notifications.pubsub import subscribe_global, listen_presence
 subscribe_global()
+listen_presence()
