fe166a5fd63f77782b78a41c91335818fdc03f53
Rohit Kumar <rohit@magicpin.in>
2017-08-01 20:32:06 +0530

    Fix database to sqlite

--- zuper/settings.py
+++ zuper/settings.py
@@ -88,24 +88,31 @@ WSGI_APPLICATION = 'zuper.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR + "/..", 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR + "/..", 'db.sqlite3'),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'zuperfast',
-            'USER': 'zuperuser',
-            'PASSWORD': 'zuperfast@123',
-            'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-            'PORT': '3306',
-        }
-    }
+}
+
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR + "/..", 'db.sqlite3'),
+#         }
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.mysql',
+#             'NAME': 'zuperfast',
+#             'USER': 'zuperuser',
+#             'PASSWORD': 'zuperfast@123',
+#             'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+#             'PORT': '3306',
+#         }
+#     }
 
 
 
